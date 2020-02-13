e6b6b93866df5be09f548bcaa782d4519880b7d0
William <william.j.t.lacroix@gmail.com>
2020-02-13 17:35:06 +0000

    remove unused components

--- src/components/Actions/Actions.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Actions } from "./Actions";
-
-export default { component: Actions, title: "Actions" };
-
-export const Default = () => <Actions />;
-Default.story = { name: "default" };
--- src/components/Reader/Reader.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Reader } from "./Reader";
-
-export default { component: Reader, title: "Reader" };
-
-export const Default = () => <Reader />;
-Default.story = { name: "default" };
