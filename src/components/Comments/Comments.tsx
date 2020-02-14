2e7f22643b110cfd367f16c6a69ba42a0f125663
Oliver Lloyd <email@oliverlloyd.com>
2020-02-14 12:10:02 +0000

    Basic pagination support

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -43,10 +43,13 @@ const getDiscussion = (
     orderBy: opts.orderBy,
     pageSize: opts.pageSize,
     displayThreaded: opts.threads !== "unthreaded",
-    maxResponses: 3
+    maxResponses: 3,
+    currentPage: opts.currentPage
   };
   const params = objAsParams(apiOpts);
   const url = baseURL + `/discussion/${shortURL}` + params;
+
+  console.log("url", url);
 
   return fetch(url)
     .then(resp => resp.json())
@@ -57,10 +60,16 @@ export const Comments = ({ shortUrl }: Props) => {
   const [comments, setComments] = useState<CommentModel[]>([]);
   const [filters, setFilters] = useState<FilterOptions>(defaultFilterOptions);
   const [loading, setLoading] = useState<boolean>(true);
+  const [pages, setPages] = useState<number>(0);
+  // const [currentPage, setCurrentPage] = useState<number>(1);
 
   const filtersUpdated = (filters: FilterOptions) => {
     setFilters(filters);
   };
+
+  // const pageUpdated = (newPage: number) => {
+  //   setCurrentPage(newPage);
+  // };
 
   const commentAdded = (comment: CommentModel) => {
     // Either we merge comments and this new comment or just make an
@@ -69,9 +78,12 @@ export const Comments = ({ shortUrl }: Props) => {
 
   useEffect(() => {
     setLoading(true);
+    console.log("effect filters", filters);
     getDiscussion(shortUrl, filters).then(json => {
+      console.log("json", json);
       setLoading(false);
       setComments(json.discussion.comments);
+      setPages(json.pages);
     });
   }, [filters]);
 
@@ -79,7 +91,7 @@ export const Comments = ({ shortUrl }: Props) => {
     <div className={containerStyles}>
       {/* <CreateComment onAdd={commentAdded} /> */}
       <TopPicks />
-      <Filters filters={filters} setFilters={filtersUpdated} />
+      <Filters filters={filters} setFilters={filtersUpdated} pages={pages} />
       {loading ? (
         <p>TODO loading component goes here...</p>
       ) : (
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -4,9 +4,9 @@ import { css } from "emotion";
 // import { palette } from "@guardian/src-foundations";
 
 type Props = {
-  pages: number[];
-  selectedpage: number;
-  onChange: () => void;
+  pages: number;
+  currentPage: number;
+  setPage: Function;
 };
 
 const containerStyles = css`
@@ -14,6 +14,34 @@ const containerStyles = css`
   flex-direction: row;
 `;
 
-export const Pagination = () => {
-  return <div className={containerStyles}>Pagination</div>;
+const buttonStyles = (selected: boolean) => css`
+  cursor: pointer;
+  width: 1.1875rem;
+  height: 1.1875rem;
+  background-color: ${selected ? "blue" : "#f6f6f6"};
+  border-radius: 62.5rem;
+  border: none;
+`;
+
+export const Pagination = ({ pages, currentPage, setPage }: Props) => {
+  // Build an array of page numbers from the total cont of pages so
+  // we can easily map over them in our jsx
+  const pageArray = [];
+  for (let page = 1; page <= pages; page++) {
+    pageArray.push(page);
+  }
+
+  return (
+    <div className={containerStyles}>
+      {pageArray.map(page => (
+        <button
+          key={page}
+          className={buttonStyles(currentPage === page)}
+          onClick={() => setPage(page)}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
 };
