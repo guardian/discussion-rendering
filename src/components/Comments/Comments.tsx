eb9ab89c3194c6a1caa855771858a999968d618e
Oliver Lloyd <email@oliverlloyd.com>
2020-02-15 12:17:54 +0000

    Use api.tsx to make the api call

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { css } from "emotion";
 
-import {
-  Comment as CommentModel,
-  DiscussionOptions,
-  DiscussionResponse
-} from "../../lib/api";
+import { Comment as CommentModel, getDiscussion } from "../../lib/api";
 
 import { CommentList } from "../CommentList/CommentList";
 import { TopPicks } from "../TopPicks/TopPicks";
@@ -18,40 +14,10 @@ type Props = {
   shortUrl: string;
 };
 
-const baseURL = "https://discussion.code.dev-theguardian.com/discussion-api";
-
 const containerStyles = css`
   display: flex;
   flex-direction: column;
 `;
-
-const objAsParams = (obj: any): string => {
-  const params = Object.keys(obj)
-    .map(key => {
-      return `${key}=${obj[key]}`; // type issues here cannot be avoided
-    })
-    .join("&");
-
-  return "?" + params;
-};
-
-const getDiscussion = (
-  shortUrl: string,
-  opts: FilterOptions
-): Promise<DiscussionResponse> => {
-  const apiOpts: DiscussionOptions = {
-    orderBy: opts.orderBy,
-    pageSize: opts.pageSize,
-    displayThreaded: opts.threads !== "unthreaded",
-    maxResponses: 3,
-    page: opts.page
-  };
-  const params = objAsParams(apiOpts);
-  const url = baseURL + `/discussion/${shortUrl}` + params;
-  return fetch(url)
-    .then(resp => resp.json())
-    .catch(error => console.error(`Error fetching ${url}`, error));
-};
 
 export const Comments = ({ shortUrl }: Props) => {
   const [comments, setComments] = useState<CommentModel[]>([]);
