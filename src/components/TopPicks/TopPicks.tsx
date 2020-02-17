6f196d869091518cdd942c057b10b62f937d8a0b
William <william.j.t.lacroix@gmail.com>
2020-02-17 16:49:33 +0000

    Top pick badges + styling

--- src/components/TopPicks/TopPicks.tsx
+++ src/components/TopPicks/TopPicks.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { css, cx } from "emotion";
 
-import { CommentType } from "../../types";
-
 import { space, neutral, palette } from "@guardian/src-foundations";
 import { textSans } from "@guardian/src-foundations/typography";
 
+import { GuardianStaff } from "../Badges/Badges";
+import { CommentType } from "../../types";
 import { avatar } from "../Comment/Comment";
 import { getPicks } from "../../lib/api";
 import { RecommendationCount } from "../RecommendationCount/RecommendationCount";
@@ -25,18 +25,20 @@ const pick = css`
   ${textSans.small()};
 `;
 
-const arrowSize = 15;
+const arrowSize = 25;
 const bg = neutral[93];
+
 const pickComment = css`
   padding: ${space[3]}px;
   background-color: ${bg};
-  border-radius: 5px;
+  border-radius: 15px;
   margin-bottom: ${arrowSize + 5}px;
   min-height: 150px;
   position: relative;
 
   :before {
     content: "";
+    margin-left: 24px;
     position: absolute;
     border-right: ${arrowSize}px solid transparent;
     border-top: ${arrowSize}px solid ${bg};
@@ -50,23 +52,7 @@ const pickMetaWrapper = css`
   padding-top: ${space[2]}px;
 `;
 
-const staffBadge = css``;
-
-const staffIcon = css`
-  display: inline-block;
-  width: 15px;
-  height: 15px;
-`;
-
-const staffLabel = css`
-  display: inline-block;
-  line-height: 15px;
-  margin: 0 0 0 ${space[1]}px;
-  ${textSans.xsmall()};
-`;
-
 const userDetails = css`
-  width: 85%;
   display: flex;
   justify-content: space-between;
 `;
@@ -80,21 +66,7 @@ const picksAvatar = css`
   margin-right: ${space[3]}px;
 `;
 
-// Staff badge
-// TODO: Are there other badges?
-const GuStaff = () => (
-  <div className={staffBadge}>
-    <svg width="36" height="36" viewBox="0 0 36 36" className={staffIcon}>
-      <path d="M18 0a18 18 0 1 0 0 36 18 18 0 0 0 0-36"></path>
-      <path
-        fill="#FFF"
-        d="M21.2 4.4c2.3.4 5.3 2 6.3 3.1v5.2H27L21.2 5v-.6zm-2.2.4c-4 0-6.3 5.6-6.3 13.2 0 7.7 2.2 13.3 6.3 13.3v.6c-6 .4-14.4-4.2-14-13.8A13.3 13.3 0 0 1 19 4v.7zm10.4 14.4l-1.9.9v8.6c-1 1-3.8 2.6-6.3 3.1V19.9l-2.2-.7v-.6h10.4v.6z"
-      ></path>
-    </svg>
-    <p className={staffLabel}>Staff</p>
-  </div>
-);
-
+// TODO: Check if there are other labels
 const Pick = ({ comment }: { comment: CommentType }) => (
   <div className={pick}>
     <div className={pickComment}>
@@ -102,6 +74,7 @@ const Pick = ({ comment }: { comment: CommentType }) => (
         className={css`
           ${textSans.small()};
           font-weight: bold;
+          margin: 0px;
         `}
       >
         Guardian Pick
@@ -118,9 +91,9 @@ const Pick = ({ comment }: { comment: CommentType }) => (
         <div className="usermeta">
           <span className={userName}>{comment.userProfile.displayName}</span>
           <Timestamp isoDateTime={comment.isoDateTime} />
-
-          {comment.userProfile.badge.filter(obj => obj["name"] === "Staff")
-            .length > 0 && <GuStaff />}
+          {comment.userProfile.badge.filter(obj => obj["name"] === "Staff") && (
+            <GuardianStaff />
+          )}
         </div>
       </div>
       <div>
