66b16bd7546122769e6fa3e6349d4d3ca741d9fb
Gareth <gtrufitt@gmail.com>
2020-02-19 08:42:59 +0000

    bye

--- --jsx/Comments.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare module "comments";
-declare type Props = {
-    shortUrl: string;
-};
-export declare const Comments: ({ shortUrl }: Props) => JSX.Element;
-export {};
