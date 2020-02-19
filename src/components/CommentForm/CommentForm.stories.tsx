fef741a290858cbde878638efc4090ebdcbc47d6
Gareth <gtrufitt@gmail.com>
2020-02-19 11:58:53 +0000

    Add userProfile to CommentForm Story

--- src/components/CommentForm/CommentForm.stories.tsx
+++ src/components/CommentForm/CommentForm.stories.tsx
@@ -4,9 +4,18 @@ import { CommentForm } from "./CommentForm";
 export default { component: CommentForm, title: "CommentForm" };
 
 const shortUrl = "/p/39f5z";
+const userProfile = {
+  userId: "2762428",
+  displayName: "FrankDeFord",
+  webUrl: "https://profile.theguardian.com/user/id/2762428",
+  apiUrl: "https://discussion.guardianapis.com/discussion-api/profile/2762428",
+  avatar: "https://avatar.guim.co.uk/user/2762428",
+  secureAvatarUrl: "https://avatar.guim.co.uk/user/2762428",
+  badge: []
+};
 
 export const Default = () => (
-  <CommentForm shortUrl={shortUrl} onAdd={() => {}} />
+  <CommentForm shortUrl={shortUrl} onAdd={() => {}} user={userProfile} />
 );
 
 Default.story = { name: "default" };
