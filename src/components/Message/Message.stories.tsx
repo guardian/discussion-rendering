5fc07b6243810400b434cc4011339adabd0ac139
Oliver Lloyd <email@oliverlloyd.com>
2020-03-02 22:00:15 +0000

    Removed dead components

--- src/components/Message/Message.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Message } from "./Message";
-
-export default { component: Message, title: "Message" };
-
-export const Default = () => <Message />;
-Default.story = { name: "default" };
