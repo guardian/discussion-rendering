f7e938de14c0ecbd3eed4a40e6dad74513ceb0ab
Oliver Lloyd <email@oliverlloyd.com>
2020-03-19 13:08:53 +0000

    Fix story format

--- src/components/LoadingComments/LoadingComments.stories.tsx
+++ src/components/LoadingComments/LoadingComments.stories.tsx
@@ -3,11 +3,5 @@ import { LoadingComments } from "./LoadingComments";
 
 export default { component: LoadingComments, title: "LoadingComments" };
 
-export const Default = () => {
-  return (
-export const Default = () => <LoadingComments />
-      <LoadingComments />
-    </>
-  );
-};
+export const Default = () => <LoadingComments />;
 Default.story = { name: "default" };
