bfd48623b468c49826dc0bc4e6574aee7fe6cf3d
Oliver Lloyd <email@oliverlloyd.com>
2020-03-29 20:46:55 +0100

    Fix date to something recent

--- src/components/Timestamp/Timestamp.stories.tsx
+++ src/components/Timestamp/Timestamp.stories.tsx
@@ -5,24 +5,24 @@ import { Timestamp } from "./Timestamp";
 
 export default { component: Timestamp, title: "Timestamp" };
 
-MockDate.set("Sun Nov 17 2019 12:00:00 GMT+0000 (Greenwich Mean Time)");
+MockDate.set("Fri March 27 2020 12:00:00 GMT+0000 (Greenwich Mean Time)");
 
 export const TwoMonths = () => (
-  <Timestamp isoDateTime={"2019-09-14T14:22:39Z"} linkTo="" />
+  <Timestamp isoDateTime={"2020-01-26T14:22:39Z"} linkTo="" />
 );
 TwoMonths.story = { name: "Two months" };
 
 export const OneHour = () => (
-  <Timestamp isoDateTime={"2019-11-17T11:00:00Z"} linkTo="" />
+  <Timestamp isoDateTime={"2020-03-27T11:00:00Z"} linkTo="" />
 );
 OneHour.story = { name: "One Hour" };
 
 export const TwentyThreeHours = () => (
-  <Timestamp isoDateTime={"2019-11-16T13:00:00Z"} linkTo="" />
+  <Timestamp isoDateTime={"2020-03-26T13:00:00Z"} linkTo="" />
 );
 TwentyThreeHours.story = { name: "Twenty three hours" };
 
 export const TwentyFiveHours = () => (
-  <Timestamp isoDateTime={"2019-11-16T11:00:00Z"} linkTo="" />
+  <Timestamp isoDateTime={"2020-03-26T11:00:00Z"} linkTo="" />
 );
 TwentyFiveHours.story = { name: "Twenty five hours" };
