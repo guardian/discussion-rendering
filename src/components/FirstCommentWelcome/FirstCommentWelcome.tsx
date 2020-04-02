bed55e6b232e84c66ad619cd4d710650833b82b0
Oliver Lloyd <email@oliverlloyd.com>
2020-04-02 17:15:49 +0100

    Add padding to the form

--- src/components/FirstCommentWelcome/FirstCommentWelcome.tsx
+++ src/components/FirstCommentWelcome/FirstCommentWelcome.tsx
@@ -58,77 +58,83 @@ export const FirstCommentWelcome = ({
   }, [body]);
 
   return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        submitForm(userName);
-      }}
+    <div
+      className={css`
+        padding: ${space[2]}px;
+      `}
     >
-      <h3
-        className={css`
-          ${headline.xxsmall({ fontWeight: "bold" })};
-        `}
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          submitForm(userName);
+        }}
       >
-        Welcome, you’re about to make your first comment!
-      </h3>
-      <p className={textStyling}>
-        Before you post, we’d like to thank you for joining the debate - we’re
-        glad you’ve chosen to participate and we value your opinions and
-        experiences.
-      </p>
-      <p className={textStyling}>
-        Please choose your username under which you would like all your comments
-        to show up. You can only set your username once.
-      </p>
-      <TextInput
-        label="Username:"
-        supporting="Must be 6-20 characters, letters and/or numbers only, no spaces."
-        value={userName}
-        onChange={e => setUserName(e.target.value)}
-        width={30}
-        error={error}
-      />
-      <p className={textStyling}>
-        Please keep your posts respectful and abide by the{" "}
-        <a
+        <h3
           className={css`
-            color: ${palette.brand[500]};
-            text-decoration: none;
-            :hover {
-              text-decoration: underline;
-            }
+            ${headline.xxsmall({ fontWeight: "bold" })};
           `}
-          href="/community-standards"
         >
-          community guidelines
-        </a>
-        {` -`} and if you spot a comment you think doesn’t adhere to the
-        guidelines, please use the ‘Report’ link next to it to let us know.
-      </p>
-      <p className={textStyling}>
-        Please preview your comment below and click ‘post’ when you’re happy
-        with it.
-      </p>
-      {previewBody && (
-        <div
-          className={cx(previewStyle, textStyling)}
-          dangerouslySetInnerHTML={{ __html: previewBody || "" }}
+          Welcome, you’re about to make your first comment!
+        </h3>
+        <p className={textStyling}>
+          Before you post, we’d like to thank you for joining the debate - we’re
+          glad you’ve chosen to participate and we value your opinions and
+          experiences.
+        </p>
+        <p className={textStyling}>
+          Please choose your username under which you would like all your
+          comments to show up. You can only set your username once.
+        </p>
+        <TextInput
+          label="Username:"
+          supporting="Must be 6-20 characters, letters and/or numbers only, no spaces."
+          value={userName}
+          onChange={e => setUserName(e.target.value)}
+          width={30}
+          error={error}
         />
-      )}
-      <div className={flexRow}>
-        <Button size="small" onClick={() => submitForm(userName)}>
-          Post your comment
-        </Button>
-        <div
-          className={css`
-            margin-left: 20px;
-          `}
-        >
-          <Button size="small" priority="tertiary" onClick={cancelSubmit}>
-            Cancel
+        <p className={textStyling}>
+          Please keep your posts respectful and abide by the{" "}
+          <a
+            className={css`
+              color: ${palette.brand[500]};
+              text-decoration: none;
+              :hover {
+                text-decoration: underline;
+              }
+            `}
+            href="/community-standards"
+          >
+            community guidelines
+          </a>
+          {` -`} and if you spot a comment you think doesn’t adhere to the
+          guidelines, please use the ‘Report’ link next to it to let us know.
+        </p>
+        <p className={textStyling}>
+          Please preview your comment below and click ‘post’ when you’re happy
+          with it.
+        </p>
+        {previewBody && (
+          <div
+            className={cx(previewStyle, textStyling)}
+            dangerouslySetInnerHTML={{ __html: previewBody || "" }}
+          />
+        )}
+        <div className={flexRow}>
+          <Button size="small" onClick={() => submitForm(userName)}>
+            Post your comment
           </Button>
+          <div
+            className={css`
+              margin-left: 20px;
+            `}
+          >
+            <Button size="small" priority="tertiary" onClick={cancelSubmit}>
+              Cancel
+            </Button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
