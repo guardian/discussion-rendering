f9d866ec94749176809af3d078aecebea6e41b3a
Oliver Lloyd <email@oliverlloyd.com>
2020-04-02 17:16:15 +0100

    A blank result is graceful

--- src/components/FirstCommentWelcome/FirstCommentWelcome.tsx
+++ src/components/FirstCommentWelcome/FirstCommentWelcome.tsx
@@ -50,7 +50,6 @@ export const FirstCommentWelcome = ({
         const response = await preview(body);
         setPreviewBody(response);
       } catch (e) {
-        // TODO: handle errors?
         setPreviewBody("");
       }
     };
