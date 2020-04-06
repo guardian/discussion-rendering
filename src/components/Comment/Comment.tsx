c45d9a035ea50c4ab3f90eb3260d138545c66a84
William Lacroix <william.lacroix.extern@joyn.de>
2020-04-06 15:56:23 +0100

    fix space type

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -217,7 +217,7 @@ const ReplyArrow = () => (
   </svg>
 );
 
-const Space = ({ amount }: { amount: number }) => (
+const Space = ({ amount }: { amount: 1 | 2 | 3 | 4 | 5 | 6 | 9 | 12 | 24 }) => (
   <div
     className={css`
       padding-right: ${space[amount]}px;
