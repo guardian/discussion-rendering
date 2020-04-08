bf187dde86665c2afe72d44a70c5b7bf0e34ae1b
Gareth <gtrufitt@gmail.com>
2020-04-08 08:34:33 +0100

    Remove Regenerator Runtime

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { css } from "emotion";
 
-import "regenerator-runtime/runtime";
-
 import { Button } from "@guardian/src-button";
 import { neutral, space } from "@guardian/src-foundations";
 import { textSans } from "@guardian/src-foundations/typography";
