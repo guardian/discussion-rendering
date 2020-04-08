b887ce92872ba334b1fffbca4ca23f3ff96f8985
Gareth <gtrufitt@gmail.com>
2020-04-08 08:39:48 +0100

    Update tsconfig, rollup and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+import pkg from "./package.json";
+import clear from "rollup-plugin-clear";
 import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
-
-const extensions = [".ts", ".tsx"];
+import visualizer from "rollup-plugin-visualizer";
 
 module.exports = {
   input: "./src/App.tsx",
@@ -16,15 +18,24 @@ module.exports = {
     }
   ],
   external: [
-    "react",
-    "react-dom",
-    "@emotion/core",
-    "@emotion/css",
-    "@guardian/src-foundations",
-    "@guardian/src-foundations/accessibility",
-    "@guardian/src-foundations/palette",
-    "@guardian/src-foundations/themes",
+    // Ignore all dependencies and PeerDependencies in build
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    // Src Foundations nested externals
     "@guardian/src-foundations/typography",
+    "@guardian/src-foundations/mq",
+    "@guardian/src-foundations/palette",
     "prop-types"
   ],
+  plugins: [
+    clear({
+      targets: ["build/"]
+    }),
+    resolve({ extensions: [".ts", ".tsx"] }),
+    typescript({
+      typescript: require("typescript")
+    }),
+    commonjs(),
+    visualizer({ filename: "build/stats.html" })
+  ]
 };
