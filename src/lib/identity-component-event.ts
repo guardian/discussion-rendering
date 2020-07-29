a84b3d88298419d2182568161e52772619339277
Peter Colley <zvvvvt@gmail.com>
2020-07-29 10:21:12 +0100

    set authentication event parameters on signin link

---
+++ src/lib/identity-component-event.ts
@@ -0,0 +1,7 @@
+
+export type AuthenticationComponentId = 'signin_to_reply_comment'
+
+export const createAuthenticationEventParams = (componentId: AuthenticationComponentId) => {
+    const params = `componentType=identityauthentication&componentId=${componentId}`;
+    return `componentEventParams=${encodeURIComponent(params)}`;
+};
