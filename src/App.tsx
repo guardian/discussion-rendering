8b2ea3a150ba17b37de3a199882d4e16e2fba748
Oliver Lloyd <email@oliverlloyd.com>
2021-01-04 15:37:59 +0000

    Set size for View more comments

--- src/App.tsx
+++ src/App.tsx
@@ -448,6 +448,7 @@ export const App = ({
 							icon={<SvgPlus />}
 							iconSide="left"
 							linkName="more-comments"
+							size="small"
 						>
 							View more comments
 						</PillarButton>
