d598185ee6175e88dab84f6ed0feffceedd26744
William Lacroix <william.j.t.lacroix@gmail.com>
2021-02-01 17:24:25 +0000

    update babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    plugins: ["const-enum", "@babel/transform-typescript"],
+    plugins: [
+        "const-enum",
+        "@babel/transform-typescript",
+        [
+            "babel-plugin-emotion",
+            {
+                "cssPropOptimization": true
+            }
+        ]
+    ],
     presets: [
         '@babel/preset-env',
         '@babel/preset-typescript',
-        '@babel/preset-react',
-        '@emotion/babel-preset-css-prop',
+        "@babel/preset-react",
+        '@emotion/babel-preset-css-prop'
     ],
-    env: {
-        test: {
-            plugins: ['@babel/plugin-transform-runtime'],
-        },
-    },
 };
