00de2cb85baeb16d83ebf050e902965a7563ef0d
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-05-09 10:14:09 +0100

    Add story for preview comment with line breaks

--- src/components/Preview/Preview.stories.tsx
+++ src/components/Preview/Preview.stories.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { css } from '@emotion/react';
 
 import { Preview } from './Preview';
 
 export default { component: Preview, title: 'Preview' };
 
 export const PreviewStory = () => (
-	<Preview previewHtml="<p>This is some preview text</p>" />
+	<div
+		css={css`
+			padding: 20px;
+			width: 700px;
+		`}
+	>
+		<Preview previewHtml="<p>This is some preview text</p>" />
+	</div>
 );
 PreviewStory.story = { name: 'default' };
+
+export const PreviewStoryLinebreaks = () => (
+	<div
+		css={css`
+			padding: 20px;
+			width: 700px;
+		`}
+	>
+		<Preview previewHtml="<p>This is some preview text<br>with a line break in the middle of a paragraph</p><p>This is a new paragraph.</p>" />
+	</div>
+);
+PreviewStoryLinebreaks.story = { name: 'Preview comment with linebreaks' };
