109ffe337676ffef1cf8db65d045cc2df7aae42e
Max Duval <max.duval@theguardian.com>
2022-10-28 12:07:54 +0100

    feat: format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,41 +4,42 @@ import visualizer from 'rollup-plugin-visualizer';
 import pkg from './package.json';
 
 /** Get build directory from package.json */
-const outDir = pkg.module.split("/")[0];
+const outDir = pkg.module.split('/')[0];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-        jsxImportSource: "@emotion/react",
-        jsxRuntime: 'classic',
-    }),
-  ],
-  build: {
-    outDir,
-    minify: false,
-    lib: {
-      entry: "./src/App.tsx",
-      formats: ["cjs","es"],
-      fileName: (format) => {
-        // Ensure the file names matches the package.json’s ones
-        const path = format === "es" ? pkg.module : pkg.main
-        // Strip out the build directory from the filename
-        return path.replace(`${outDir}/`, "");
-      },
-    },
-    rollupOptions: 
-      {
-        input: './src/App.tsx',
-        external: [
-          // Ignore all dependencies and PeerDependencies in build
-          ...Object.keys(pkg.peerDependencies || {}),
-          'prop-types',
-        ],
-        plugins: [
-          //@ts-expect-error -- it works
-          visualizer({ filename: 'build/stats.html' }),
-        ],
-    },
-  }
+	plugins: [
+		react({
+			jsxRuntime: 'classic',
+			babel: {
+				plugins: ['@emotion/babel-plugin'],
+			},
+		}),
+	],
+	build: {
+		outDir,
+		minify: false,
+		lib: {
+			entry: './src/App.tsx',
+			formats: ['cjs', 'es'],
+			fileName(format) {
+				// Ensure the file names matches the package.json’s ones
+				const path = format === 'es' ? pkg.module : pkg.main;
+				// Strip out the build directory from the filename
+				return path.replace(`${outDir}/`, '');
+			},
+		},
+		rollupOptions: {
+			input: './src/App.tsx',
+			external: [
+				// Ignore all dependencies and PeerDependencies in build
+				...Object.keys(pkg.peerDependencies || {}),
+				'prop-types',
+			],
+			plugins: [
+				//@ts-expect-error -- it works
+				visualizer({ filename: 'build/stats.html' }),
+			],
+		},
+	},
 });
