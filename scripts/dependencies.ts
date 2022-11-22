43c8c8eaa27e59593c8e00278493a3209c623a9f
Max Duval <max.duval@theguardian.com>
2022-11-22 11:11:58 +0000

    feat(deps): pin dev to lowest of peer
    This is the recommended way to dealing with dependencies,
    as outlined in our docs:
    https://github.com/guardian/recommendations/blob/main/npm-packages.md#peerdependencies

--- scripts/dependencies.ts
+++ scripts/dependencies.ts
@@ -1,20 +1,28 @@
 import pkg from "../package.json" assert { type: "json" };
 
+const isDevDep = (dep: string): dep is keyof typeof pkg.devDependencies =>
+  dep in pkg.devDependencies;
+
 const mismatches = Object.entries(pkg.peerDependencies).map(([name, peer]) => {
-  // @ts-expect-error
+  if (!isDevDep(name)) return (`Missing dev dependency ${name}`);
+
   const dev = pkg.devDependencies[name];
   if (typeof dev !== "string" || typeof peer !== "string") {
-    console.error("Could not parse semver range:", name, {dev, peer});
-    return [dev, peer].join(", ");
+    return `Could not parse semver range for ${name}: dev@${dev}, peer@${peer}`;
   }
-  if (dev !== peer) {
-    console.error("Mismatch in peer dependency versions:", name, {dev, peer});
-    return [dev, peer].join(", ");
+
+  if (`^${dev}` !== peer) {
+    return `Mismatch in \`${name}\` dependency: dev@${dev}, peer@${peer}`;
   }
 
   return false;
 }).filter(Boolean);
 
 if (mismatches.length > 0) {
+  console.error(mismatches);
   Deno.exit(1);
+} else {
+  console.info("Dev and peer dependencies are correctly aligned:");
+  console.info(pkg.peerDependencies);
+  Deno.exit();
 }
